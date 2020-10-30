98af3b87ff5dfea3b055cc3df7a338e040d325e0
TamMelPer <mystic101@gmail.com>
2020-10-30 13:49:22 +0000

    TDD for all 6 User Stories

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,11 @@
 class Dice
   def roll(n = 1)
-    n.times {rand(1..6)}
+    roll_history = []
+    n.times { roll_history << rand(1..6)}
+    return roll_history
+  end
+
+  def total(roll)
+    roll.reduce(:+)
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,25 +1,37 @@
 require 'dice'
 
 describe Dice do
+# User Story 1 - I want to be able to roll a dice
   it 'creates an instance of dice' do
     expect(subject).to be_instance_of Dice
   end
 
+# User Story 2 - Rolling a dice, should return a number between 1-6
+# User Story 3 - I want the dice roll to be randomly picked
+    # it 'returns a random number between 1-6' do
+    #   expect(subject.roll). to be_between(1, 6)
+    # end
+
+# User Story 4 - I want to be able to roll any number of dice
   it "responds to a roll with one argument" do
     expect(subject).to respond_to(:roll).with(1).argument
   end
+  # it "give the right number of result" do
+  #   expect(subject.roll(7)).to eq 7
+  # end
 
-  it "give the right number of result" do
-    expect(subject.roll(7)).to eq 7
+
+# User Story 5 - To keep track, I want to record each dice roll
+  it "give all the results between 1 and 6 for the number of rolls" do
+    expect(subject.roll(10)).to all( be_between(1,6) )
   end
 
-  it 'returns a reandom number between 1-6' do
-    expect(subject.roll). to be_between(1, 6)
+# User Story 6 - I want to be able to get my current score
+  it "gives a total of all the rolled dice" do
+    expect(subject.total([1, 2, 3])).to eq(6)
   end
+
 
 
 
 end
-
-
-#it { is_expected.to respond_to(:roll).with(1).argument }
